5baf3e46e9c69519ce66da1dba9309f5693e8973
jfchen19 <jchenf19@gmail.com>
2020-12-21 21:23:04 +0800

    complete

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,50 +8,37 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-// import 'jquery'
+import 'jquery'
 import 'popper.js'
 import 'bootstrap'
 import 'bootstrap/scss/bootstrap.scss'
 
 import 'select2'
 import 'select2/dist/css/select2.css'
-// import $ from 'jquery' 
+import $ from 'jquery' 
 window.$ = $
-// currency
-$(document).on('turbolinks:load', function() {
-  $('.js-example-basic-single').select2();  // currency
-  $(".js-example-tags").select2({           // tags (turbolinks)
-    tags: true
-  });
-});
-
 import 'sweetalert2'
+import 'sweetalert2/dist/sweetalert2.css'
 import Swal from 'sweetalert2'
 window.Swal = Swal
+import 'tempusdominus-bootstrap-4'
+import 'tempusdominus-bootstrap-4/build/css/tempusdominus-bootstrap-4.css'
+import 'font-awesome/css/font-awesome.css'
 
-document.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('.btn-submit').addEventListener('click', function(e) {
-    e.preventDefault()
-    Swal.fire({
-      title: 'Do you want to save the changes?',
-      showDenyButton: true,
-      showCancelButton: true,
-      confirmButtonText: `Save`,
-      denyButtonText: `Don't save`,
-    }).then((result) => {
-      if (result.isConfirmed) {
-        Swal.fire({
-          title: 'loading...',
-          showConfirmButton: false
-        })
-       document.querySelector('form').submit()
-      }
-    })
-  })
-
+$(document).on('turbolinks:load', function() {
+  // currency
+  $('.js-example-basic-single').select2();
+  // tags
+  $(".js-example-tags").select2({
+    tags: true
+  });
+  // date
   $(function () {
-    $('#datetimepicker7').datetimepicker();
+    $('#datetimepicker7').datetimepicker({
+      format: 'YYYY/MM/DD'
+    });
     $('#datetimepicker8').datetimepicker({
+      format: 'YYYY/MM/DD',
       useCurrent: false
     });
     $("#datetimepicker7").on("change.datetimepicker", function (e) {
@@ -61,12 +48,28 @@ document.addEventListener('DOMContentLoaded', function() {
       $('#datetimepicker7').datetimepicker('maxDate', e.date);
     });
   });
-})
-
-import 'tempusdominus-bootstrap-4'
-import 'tempusdominus-bootstrap-4/build/css/tempusdominus-bootstrap-4.css'
-import 'font-awesome/css/font-awesome.css'
-import { loaders } from '@rails/webpacker'
+  // confirm
+  $('.btn-submit').on('click',function(e){
+    e.preventDefault();
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: 'Loading...',
+          showConfirmButton: false
+        })
+        document.querySelector('form').submit()
+      }
+    })
+  }) 
+});
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
